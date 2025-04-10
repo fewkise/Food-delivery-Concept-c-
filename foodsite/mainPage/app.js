8328ca502ab3940aa08d76a026cfdfe5696ad0a6
nyashka <oleshakirsanova@gmail.com>
2025-04-10 23:19:00 +0200

    AddingSwiper

--- foodsite/mainPage/app.js
+++ foodsite/mainPage/app.js
@@ -66,3 +66,49 @@ opens.addEventListener('click', () => {
 });
 
 modalopen();
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+      speed:1200,
+    slidesPerView: 4,
+    spaceBetween: 10,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  const swiper1 = new Swiper('.swiper1', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed:900,
+    slidesPerView: 8,
+    spaceBetween: 10,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
